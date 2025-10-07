b2ea9a5730ee1d520f780fe554f0c3dd06d5b737
rupak26 <rupakbiswas29bd@gmail.com>
2025-10-08 01:16:29 +0600

    websocket  implemented

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/rupak26/WebSockets_in_Golang
 
 go 1.22.3
+
+require github.com/gorilla/websocket v1.5.3 // indirect
--- main.go
+++ main.go
@@ -2,22 +2,70 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
+	//"golang.org/x/text/message"
 )
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize: 1024,
+	WriteBufferSize: 1024,
+} 
+
+func reader(conn *websocket.Conn) {
+	for {
+		messageType , p , err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		log.Println(string(p)) 
+
+		if err := conn.WriteMessage(messageType , p) ; err != nil {
+           log.Println(err)
+		   return
+		}
+	}
+}
+
 
 func homepage(w http.ResponseWriter , r *http.Request) {
     fmt.Fprintf(w , "Home Page")
 }
 
 func wsEndpoints(w http.ResponseWriter , r *http.Request) {
-	  fmt.Fprintf(w , "Web Socket Endpoints")
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+
+	ws , err := upgrader.Upgrade(w , r , nil) 
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println("Client Successfully Connected")
+	reader(ws)
 }
 
-func setUpRoutes() {
-	http.HandleFunc("/", homepage)
-	http.HandleFunc("/ws", wsEndpoints)
+func setUpRoutes(mux *http.ServeMux) {
+	mux.HandleFunc("/", homepage)
+	mux.HandleFunc("/ws", wsEndpoints)
 }
 
 func main() {
-	setUpRoutes()
+	
+	mux := http.NewServeMux() 
+    
+    setUpRoutes(mux)
+	
+	port := ":8080" 
+    
+	fmt.Println("Server is running on port 8080")
+
+	err := http.ListenAndServe(port , mux) 
+
+	if err != nil {
+		fmt.Println("Server is Facing Issues")
+	}
+
 }
